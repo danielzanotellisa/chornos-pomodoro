609caf7a1e7a866a15f5aa84d1b2c5516dd8276c
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-03 18:30:50 -0300

    refactor: refatora a chamada do componente `Logo`

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { TimerIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import './styles/global.css';
 import './styles/theme.css'
-import { HeadingComponent } from './components/Heading';
+
 export function App() {
     return (
         <>
             <Container>
-                <Logo>
-                    <TimerIcon size={56}/>
-                    <HeadingComponent>Chronos</HeadingComponent>
-                </Logo>
+                <Logo/>
             </Container>
             <Container>
                 <section>MENU</section>
