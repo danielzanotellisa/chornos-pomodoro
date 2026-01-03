10056229faab72416386fe40a12678d2a809b752
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-03 00:42:14 -0300

    refactor: cria e atribui um novo tipo `HeadingProps` para que seja aceito vários `children` em um HeadingComponent

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,11 @@
 import styles from  './Heading.module.css';
 
-export function HeadingComponent(props) {
+type HeadingProps = {
+    children: React.ReactNode;
+}
+
+export function HeadingComponent({ children }: HeadingProps) {
     return (<>
-        <h1 className={styles.heading}>{props.children}</h1>
+        <h1 className={styles.heading}>{children}</h1>
     </>);
 }
