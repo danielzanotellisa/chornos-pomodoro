3bb31422680604e10cada5608bb46e1cbdb0c2c4
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-01-03 00:42:38 -0300

    chore: testa e refatora o `HeadingComponent`

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,19 @@ import { useState } from "react";
 import './styles/global.css';
 import './styles/theme.css'
 import {HeadingComponent} from './components/Heading.tsx'
+import { TimerIcon } from "lucide-react";
 export function App() {
     return (
         <>
-            <HeadingComponent> Texto personalizado</HeadingComponent>
+            <HeadingComponent> 
+                Texto personalizado
+                <button>
+                    <TimerIcon />
+                </button>
+            </HeadingComponent>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam error, sint provident accusantium aperiam quia quidem impedit fugit molestias cum adipisci consectetur laudantium eligendi sapiente, magni laborum, minima numquam suscipit!</p>
-            <Counter />
         </>
     );
     
-}
-
-function Counter() {
-    const [number, setNumber] = useState(0);
-    
-    return (
-        <>
-            <p>Contador: {number}</p>
-            <button onClick={() => setNumber((number) => number + 1)}> Aumentar em 1</button>
-            <button onClick={() => setNumber(0)}>Resetar</button>
-        </>
-    );
 }
 
